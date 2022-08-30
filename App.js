983d10d2462b2799f248412ebc994ae721e1e669
Battug <101857782+Battug@users.noreply.github.com>
2022-08-30 16:02:42 +0800

    Third commit

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen'
